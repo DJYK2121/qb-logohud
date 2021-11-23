c841790cf4d468d885213ac34bde237014653c06
unknown <merpindia1@gmail.com>
2021-11-24 03:57:30 +0530

    First

---
+++ html/script.js
@@ -0,0 +1,25 @@
+$(document).ready(function () {
+  $("#page").hide();
+});
+
+window.addEventListener("message", function (event) {
+  var edata = event.data;
+
+  if (edata.type == "toggle") {
+    if (edata.value) {
+      $("#page").show();
+    } else {
+      $("#page").hide();
+    }
+  }
+
+  if (edata.type == "update") {
+    $("#page").show();
+    $("#u720-4").text(edata.online);
+    $("#u753-4").text(edata.id);
+    $("#u762-4").text(edata.money);
+    $("#u771-4").text(edata.bank);
+    $("#u780-4").text(edata.playtime);
+    $("#u111-6").text(edata.date);
+  }
+});
